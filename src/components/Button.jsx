b40b829db6b7ea2e0d84d9aaa2434e6f8e759951
calvincho95 <calvincho95@naver.com>
2024-05-10 14:54:29 +0900

    3rd commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ name }) => {
+const Button = ({ name, result, setResult }) => {
+  const onClickButton = () => {
+    if (result.length >= 17) return;
+
+    if (result === "0") {
+      setResult(name);
+    } else {
+      setResult(result + name);
+    }
+  };
+
   return (
-    <button className="bg-green-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm">
+    <button
+      className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      onClick={onClickButton}
+    >
       {name}
     </button>
   );
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,5 +1,5 @@
-const Result = () => {
-  return <div className=" bg-white-100 p-2 rounded-md shadow-sm">Result</div>;
+const Result = ({ result }) => {
+  return <div className=" bg-red-100 p-2 rounded-md shadow-sm">{result}</div>;
 };
 
 export default Result;
